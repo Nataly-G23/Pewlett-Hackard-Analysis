ac3b68cdec200129824bda482650782c381dc02d
Nataly Granados <104810450+Nataly-G23@users.noreply.github.com>
2022-06-23 16:46:41 -0600

    Update Employees_Database_challenge.sql

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employees_Database_challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employees_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Use Dictinct with Orderby to remove duplicate rows
 SELECT e.emp_no, 
 	e.first_name,
 	e.last_name,
@@ -11,8 +12,7 @@ on (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
---REMOVE DUPLICATES
-	
+--REMOVE DUPLICATES	
 SELECT DISTINCT ON (r.emp_no) r.emp_no,
 	r.first_name,
 	r.last_name,
@@ -28,3 +28,20 @@ INTO retiring_titles
 FROM unique_titles as u 
 GROUP BY u.title
 ORDER BY count DESC;
+
+--Mentorship Eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	d.from_date,
+	d.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+	INNER JOIN dept_employees as d
+	ON (e.emp_no = d.emp_no)
+	INNER JOIN titles as ti
+	on (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
